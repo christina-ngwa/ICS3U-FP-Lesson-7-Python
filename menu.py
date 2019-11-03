cab374b228b7f83855624a6512ad125cb859bac1
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-11-03 03:31:56 +0000

    1

--- menu.py
+++ menu.py
@@ -15,30 +15,33 @@ def menu_scene():
     # This function is the splash game loop
 
     # New pallet for black filled text
-    NEW_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-                   b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+    NEW_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff
+                   '\xff'b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff
+                   '\xff\xff\xff')
 
     # an image bank for CircuitPython
     image_bank_1 = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # set the background to image 0 in the bank
-    background = stage.Grid(image_bank_1, constants.SCREEN_X, constants.SCREEN_Y)
+    background = stage.Grid(image_bank_1, constants.SCREEN_X,
+                            constants.SCREEN_Y)
 
     # a list of sprites that wil be updated every frame
     sprites = []
 
     # add text objects
     text = []
-    text1 = stage.Text(width = 29, height = 12, font = None, palette = NEW_PALETTE, buffer = None)
+    text1 = stage.Text(width=29, height=12, font=None,
+                       palette=NEW_PALETTE, buffer=None)
     text1.move(20, 10)
     text1.text("MT Game Studios")
     text.append(text1)
 
-    text2 = stage.Text(width = 29, height = 12, font = None, palette = NEW_PALETTE, buffer = None)
+    text2 = stage.Text(width=29, height=12, font=None,
+                       palette=NEW_PALETTE, buffer=None)
     text2.move(40, 110)
     text2.text("PRESS START")
     text.append(text2)
-
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
@@ -63,7 +66,8 @@ def menu_scene():
 
         # redraw sprite list
         # game.render_sprites()
-        game.tick() # wait until refresh rate finishes
+        game.tick()  # wait until refresh rate finishes
+
 
 def game_scene():
     # This function is a scene
@@ -143,7 +147,6 @@ def game_scene():
         # redraw sprite list
         game.render_sprites(sprites)
         game.tick()  # wait until refresh rate finishes
-
 
 
 if __name__ == "__main__":
